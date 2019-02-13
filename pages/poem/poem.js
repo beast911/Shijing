6aceec7dbc385e5f3a799e71fffd2425e2958f4c
Kanwar Udaybir SINGH <kanwarudaybir.singh@amadeus.com>
2019-02-13 10:11:39 +0100

    updated data file

--- pages/poem/poem.js
+++ pages/poem/poem.js
@@ -6,34 +6,42 @@ Page({
    */
   data: {
     poem: {},
-    tabItems: [
-      {
-        id: "simple",
+    tabItems: {
+      "simple": {
+        text: ""
+      },
+      "note": {
         text: "",
         selected: "active"
       },
-      {
-        id: "note",
-        text: ""
-      },
-      {
-        id: "analysis",
+      "analysis": {
         text: ""
       }
-    ],
+  },
     details: ""
   },
 
   loadDetails: function(e) {
+    for (let prop in this.data.tabItems) {
+      if (this.data.tabItems.hasOwnProperty(prop)) {
+        if (e.currentTarget.id !== prop) {
+          delete this.data.tabItems[prop].selected
+        }
+      }
+    }
     switch (e.currentTarget.id) {
+      // pick the category from data loaded
       case "simple": 
         this.data.details = "Simple";
+        this.data.tabItems["simple"].selected = "active";
         break;
       case "analysis":
-        this.data.details = "Analysis";
+        this.data.details = this.data.poem.analysis;
+        this.data.tabItems["analysis"].selected = "active";
         break;
       case "note":
         this.data.details = "Note";
+        this.data.tabItems["note"].selected = "active";
         break;
     };
     this.setData(this.data);
